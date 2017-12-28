0c1f53af0d07fa955507eae18e00868ae91c5db7
liangjianhui <525775974@qq.com>
2017-12-28 15:27:05 +0800

    1

---
+++ src/zhaoyin/ZhaoYin.java
@@ -0,0 +1,13 @@
+package zhaoyin;
+
+/**
+ * Created by Administrator on 2017/12/28.
+ */
+public class ZhaoYin {
+    public static void main(String[] args) {
+        String name = "LiangJianHui";
+        int salary = 9980;
+        System.out.println(name+"的工资是"+salary);
+
+    }
+}
